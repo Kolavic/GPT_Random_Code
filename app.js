5c03691bd73e09ba66872aa3ca77f113b8a83812
Kolavic Kay <kolavickay@gmail.com>
2023-12-30 06:50:11 +0100

    First Commit (aka init)

---
+++ app.js
@@ -0,0 +1,55 @@
+// app.js
+// This is a simplified JavaScript file for demonstration purposes.
+
+// Sample product data
+const products = [
+    { id: 1, name: 'Product 1', price: 19.99 },
+    { id: 2, name: 'Product 2', price: 29.99 },
+    // ... Add more products
+  ];
+  
+  // Sample shopping cart data
+  let shoppingCart = [];
+  
+  // Function to display products
+  function displayProducts() {
+    const productContainer = document.querySelector('.product-list');
+    productContainer.innerHTML = '';
+  
+    products.forEach(product => {
+      const productElement = document.createElement('div');
+      productElement.innerHTML = `
+        <h3>${product.name}</h3>
+        <p>$${product.price}</p>
+        <button onclick="addToCart(${product.id})">Add to Cart</button>
+      `;
+      productContainer.appendChild(productElement);
+    });
+  }
+  
+  // Function to add a product to the shopping cart
+  function addToCart(productId) {
+    const selectedProduct = products.find(product => product.id === productId);
+    if (selectedProduct) {
+      shoppingCart.push(selectedProduct);
+      displayShoppingCart();
+    }
+  }
+  
+  // Function to display the shopping cart
+  function displayShoppingCart() {
+    const cartContainer = document.querySelector('.shopping-cart');
+    cartContainer.innerHTML = '';
+  
+    shoppingCart.forEach(product => {
+      const cartItem = document.createElement('div');
+      cartItem.innerHTML = `
+        <p>${product.name} - $${product.price}</p>
+      `;
+      cartContainer.appendChild(cartItem);
+    });
+  }
+  
+  // Initial setup
+  displayProducts();
+  
